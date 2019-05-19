05c584f3482fe6f535715f68d2a5ddd17824a87b
plajuan <pla.juan@gmail.com>
2019-05-19 17:00:22 -0300

    using handle bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+
+app.set('view engine', 'hbs')
+app.set('views', `${__dirname}/templates/views`)
+hbs.registerPartials(`${__dirname}/templates/partials`)
 
 app.get('', (req, res)=>{
-    res.sendFile(__dirname + "/" + "index.html")
+    //res.sendFile(__dirname + "/" + "index.html")
+    res.render('index')
 })
 
 app.listen(3000)
